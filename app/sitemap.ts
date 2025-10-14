7ed091e0c1e322bcd871e62cbccf805aa377ec25
paperpatch-shop <paperpatch.artshop@gmail.com>
2025-10-14 22:00:03 +0600

    Update sitemap to use paperpatch.shop domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://paperpatch.vercel.app'
+  const baseUrl = 'https://paperpatch.shop'
 
   return [
     {
